9d4856b50d9c8197b43ab8653584eda3bdb5816a
emilybaum <emily.r.baum@gmail.com>
2019-05-11 19:15:36 -0400

    some pseudo code and placeholder HTML elements with ids

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+// CRYSTAL NUMBER GENERATOR
+// generate random number generated for crystal: 1-12 --- do this 4 times
+// assign each random number to a diffenet crystal variable
+// assign value to an gem on HTML
+
+// GAME NUMBER GENERATOR
+// random number generates to reach: 19-120
+// Assign number to a variable
+// display the game number on HTML
+
+// PLAYING THE GAME
+// listen for button clicks on the gems
+// when a button is clicked, update the guesses value/variable
+// continue to add together the numbers for the crystals that are clicked
+// display sum on the page for guesses
+
+// HAS WON
+// if the sum for guesses === the original game value, win
+// call a function for updating wins/losses
+// also 
+
+// HAS LOST
+// if the sum for gussess > the original value, lose
+// call a function for updating wins/losses
+
+// TRACKING WINS AND LOSSES
+// if win, +=1 to Wins categoty
+// if loss, +=1 to losses category
+// call function for starting a new round
+
+// STARTING A NEW ROUND
+// call funciton for generating new crystal number
+// call funciton for generating new random number
+
+
+
+
+
+
+
+
+// EXTRA----------
+    // EVALUATE THE BEST COMBINATION WITH THE LEAST AMOUNT OF CLICKS
