0b8772988f315a9ccc6d8d3e9e5522177e3448e9
emilybaum <emily.r.baum@gmail.com>
2019-05-12 13:55:56 -0400

    added random  value to each div for crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,61 @@
 
-// $(document).ready(function () {
-// var crystalArray = [];
-var crystal1 = crystalNumber();
-var crystal2 = crystalNumber();
-var crystal3 = crystalNumber(); 
-var crystal4 = crystalNumber();
+$(document).ready(function () {
 var gameNum = 0;
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
+    // var gettingCrystalsNumbers = valToCrystalNumber();
 
-// CRYSTAL NUMBER GENERATOR OPTION 2
-function crystalNumber() {
-    // var array = [] SHOULD I BE USING AN ARRAY HERE ????
+$("#your-number").html(gameNum);
+$("#wins").html(wins);
+$("#losses").html(losses);
+$("#total-score").html(totalScore);
+
+// RANDOM NUMBER FOR CRYSTALS
+function randomCrystalNum() {
     var random = Math.floor(Math.random() * 12) + 1;
-    // SHOULD I ASSIGN .val() to element ID here?
     return random;
 }
+
+// CRYSTAL NUMBER GENERATOR OPTION 2
+function valToCrystalNumber() {
+    var array = ["#crystal1", "#crystal2", "#crystal3", "#crystal4"]
+    for (var i = 0; i < array.length; i++) {
+        $(array[i]).attr("data-crystalvalue", randomCrystalNum());
+    }
+}
+valToCrystalNumber();
 
 // GAME NUMBER GENERATOR
 function gameNumber() {
     var gameNum = Math.floor(Math.random() * 101) + 19;
     return gameNum;
 }
+gameNumber();
 
-// SET UP VALUES FOR CRYSTALS
-// display the crystal values on the html page using .val()
-// set total to 0
+
+$(".crystals").on("click", function () {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    totalScore += crystalValue;
+
+    // All of the same game win-lose logic applies. So the rest remains unchanged.
+    alert("New score: " + totalScore);
+
+    if (totalScore === gameNum) {
+        alert("You win!");
+    }
+
+    else if (totalScore >= gameNum) {
+        alert("You lose!!");
+    }
+
+});
+
+
+
+
+
 
 
 
@@ -34,52 +63,39 @@ function gameNumber() {
 // when an image is selected, add that value to the totalScore
 // return totalScore
 
-
-
 // HAS WON
 // if the sum for guesses === the original game value, win
 // call a function for updating wins/losses
-function isWinner(gameNum, totalScore) {
-    if (gameNum === totalScore) {
-        wins +=1;
-        updateHTML()
-        return true;
-    }
-}
+// function isWinner(gameNum, totalScore) {
+//     if (gameNum === totalScore) {
+//         wins +=1;
+//         updateHTML()
+//         return true;
+//     }
+// }
 
 
 // HAS LOST
 // if the sum for gussess > the gameNum, lose
 // call a function for updating wins/losses
-function isLoser(gameNum, totalScore) {
-    if (totalScore > gameNum) {
-        losses +=1;
-        updateHTML()
-        return true;
-    }
-}
+// function isLoser(gameNum, totalScore) {
+//     if (totalScore > gameNum) {
+//         losses +=1;
+//         updateHTML()
+//         return true;
+//     }
+// }
 
 // PLAYING THE GAME
 // listen for button clicks on the gems
 // when a button is clicked, update the guesses value/variable
 // continue to add together the numbers for the crystals that are clicked
 // display sum on the page for guesses
-$("#img-pink-gem").click(function () {
-    totalScore += crystal1;    
-})
-
-$("#img-cupcake").on("click", function () {
-
-})
-
-$("#img-aztec").click(function () {
-
-})
 
 
-$("#img-ice-cube").click(function () {
 
-})
+
+
 
 // STARTING A NEW ROUND
 // call funciton for generating new crystal number
@@ -89,16 +105,10 @@ $("#img-ice-cube").click(function () {
 // UPDATE HTML
 // display the game number on HTML
 // give value to each image on HTML
-function updateHTML() {
-    $("#your-number").html(gameNum);
-    $("#wins").html(wins);
-    $("#losses").html(losses);
-    $("#total-score").html(totalScore);
-}
 
 
 
-// });
+});
 
 
 // EXTRA----------
