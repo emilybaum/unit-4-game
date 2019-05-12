a0f333ff6252682b5cf36a7aa3f68c7909629c28
emilybaum <emily.r.baum@gmail.com>
2019-05-12 14:11:38 -0400

    game is functional, but the new round does not start until you click on another image - I want the game to reset as soon as the round is won or lost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,114 +1,75 @@
 
 $(document).ready(function () {
-var gameNum = 0;
-var wins = 0;
-var losses = 0;
-var totalScore = 0;
-    // var gettingCrystalsNumbers = valToCrystalNumber();
+    var gameNum = gameNumber();
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
 
-$("#your-number").html(gameNum);
-$("#wins").html(wins);
-$("#losses").html(losses);
-$("#total-score").html(totalScore);
+    // UPDATES TO THE HTML ELEMENTS
+    $("#your-number").html(gameNum);
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+    $("#total-score").html(totalScore);
 
-// RANDOM NUMBER FOR CRYSTALS
-function randomCrystalNum() {
-    var random = Math.floor(Math.random() * 12) + 1;
-    return random;
-}
-
-// CRYSTAL NUMBER GENERATOR OPTION 2
-function valToCrystalNumber() {
-    var array = ["#crystal1", "#crystal2", "#crystal3", "#crystal4"]
-    for (var i = 0; i < array.length; i++) {
-        $(array[i]).attr("data-crystalvalue", randomCrystalNum());
-    }
-}
-valToCrystalNumber();
-
-// GAME NUMBER GENERATOR
-function gameNumber() {
-    var gameNum = Math.floor(Math.random() * 101) + 19;
-    return gameNum;
-}
-gameNumber();
-
-
-$(".crystals").on("click", function () {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    totalScore += crystalValue;
-
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + totalScore);
-
-    if (totalScore === gameNum) {
-        alert("You win!");
+    // RANDOM NUMBER FOR CRYSTALS
+    function randomCrystalNum() {
+        var random = Math.floor(Math.random() * 12) + 1;
+        return random;
     }
 
-    else if (totalScore >= gameNum) {
-        alert("You lose!!");
+    // CRYSTAL NUMBER GENERATOR OPTION 2
+    function valToCrystalNumber() {
+        var array = ["#crystal1", "#crystal2", "#crystal3", "#crystal4"]
+        for (var i = 0; i < array.length; i++) {
+            $(array[i]).attr("data-crystalvalue", randomCrystalNum());
+        }
+    }
+    valToCrystalNumber();
+
+    // GAME NUMBER GENERATOR
+    function gameNumber() {
+        var gameNum = Math.floor(Math.random() * 101) + 19;
+        return gameNum;
     }
 
+
+    // PLAYING HTE GAME - REGISTERS CLICKS
+    $(".crystals").on("click", function () {
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        totalScore += crystalValue;
+        $("#total-score").html(totalScore);
+        
+        if (totalScore === gameNum) {
+            wins +=1;
+            $("#wins").html(wins);
+            alert("You win!");
+            startNewRound();
+        }
+
+        else if (totalScore >= gameNum) {
+            losses += 1;
+            $("#losses").html(losses);
+            alert("You lose!!");
+            startNewRound();
+        }
+    
+    })
+
+    // START A NEW ROUND
+    function startNewRound() {
+        valToCrystalNumber();
+        gameNumber();
+        totalScore = 0
+    }
 });
 
+// 
+// get new game number
+// new crystal values
 
 
 
-
-
-
-
-// CALCULATING SCORE
-// when an image is selected, add that value to the totalScore
-// return totalScore
-
-// HAS WON
-// if the sum for guesses === the original game value, win
-// call a function for updating wins/losses
-// function isWinner(gameNum, totalScore) {
-//     if (gameNum === totalScore) {
-//         wins +=1;
-//         updateHTML()
-//         return true;
-//     }
-// }
-
-
-// HAS LOST
-// if the sum for gussess > the gameNum, lose
-// call a function for updating wins/losses
-// function isLoser(gameNum, totalScore) {
-//     if (totalScore > gameNum) {
-//         losses +=1;
-//         updateHTML()
-//         return true;
-//     }
-// }
-
-// PLAYING THE GAME
-// listen for button clicks on the gems
-// when a button is clicked, update the guesses value/variable
-// continue to add together the numbers for the crystals that are clicked
-// display sum on the page for guesses
-
-
-
-
-
-
-// STARTING A NEW ROUND
-// call funciton for generating new crystal number
-// call funciton for generating new random number
-
-
-// UPDATE HTML
-// display the game number on HTML
-// give value to each image on HTML
-
-
-
-});
 
 
 // EXTRA----------
